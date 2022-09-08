17197989f08112f2b6bb7d04e18b31fcde7b975b
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-09-08 14:54:36 -0600

    good things to reference when troubleshooting my projects

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const dogs = [
-  { name: "Snickers", age: 2 },
-  { name: "hugo", age: 8 },
+  { name: "Odie", age: 3 },
+  { name: "Tess", age: 1 },
 ];
 
 function makeGreen() {
@@ -10,25 +10,60 @@ function makeGreen() {
 }
 
 // Regular
+console.log("hello");
 
 // Interpolated
+console.log("Hello I am a %s string!", "silly");
 
 // Styled
+console.log(
+  "%c I am a piece of text",
+  "font-size: 20px; background: blue; text-shadow: 2px 2px 0 gray"
+);
 
 // warning!
+console.warn("Rut-ro");
 
 // Error :|
+console.error("ERROR");
 
 // Info
+console.info("My dogs name is Odie");
 
 // Testing
+// console.assert(1 === 1, "That is incorrect");
+const p = document.querySelector("p");
+
+console.assert(p.classList.contains("foo"), "That is is incorrect");
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p); //shows all the available methods on that element
 
 // Grouping together
+dogs.forEach((dog) => {
+  //   console.group(`${dog.name}`);
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} in dog years`);
+  console.groupEnd(`${dog.name}`);
+});
 
 // counting
+console.count("Ted");
+console.count("Ted");
+console.count("Ted");
+console.count("Ted");
 
 // timing
+console.time("fetch data");
+fetch("https://api.github.com/users/ruxpin86")
+  .then((data) => data.json())
+  .then((data) => {
+    console.timeEnd("fetch data");
+    console.log(data);
+  });
